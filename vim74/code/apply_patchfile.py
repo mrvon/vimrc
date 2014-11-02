0d3437e4c46e1df9cd5bcd4b1feb885dd8e58381
Mrvon <mrvon@qq.com>
2014-11-02 11:11:34 +0800

    use python rewrite download_patchfile.py and apply_patchfile.py

--- vim74/code/apply_patchfile.py
+++ vim74/code/apply_patchfile.py
@@ -4,15 +4,19 @@
 
 import os
 
-min_patch_num  = 1
-max_patch_num  = 450
+min_patch_num = 1
+max_patch_num = 450
 
 for i in range(min_patch_num, max_patch_num):
-    patch_file = "7.4.{0:003}".format(i)
+    patch_file = "7.4.{0:03}".format(i)
     print("Start patch file: " + patch_file)
     patch_cmd = "patch -p0 < " + patch_file
-    if os.system(patch_cmd) == 0:
-        print("Patch file {0} SUCCESSFUL".format(patch_file))
-    else:
-        print("Patch file {0} FAILED".format(patch_file))
+
+    try:
+        if os.system(patch_cmd) == 0:
+            print("Patch file {0} SUCCESSFUL".format(patch_file))
+        else:
+            print("Patch file {0} FAILED".format(patch_file))
+            break
+    except KeyboardInterrupt:
         break
